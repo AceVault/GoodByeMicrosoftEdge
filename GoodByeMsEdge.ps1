364ef487e26bc2d87a33af0be99ef990215d0ea6
AceVault <96090803+AceVault@users.noreply.github.com>
2022-12-24 22:37:09 -0500

    Add files via upload

---
+++ GoodByeMsEdge.ps1
@@ -0,0 +1,10 @@
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+     Exit
+    }
+   }
+taskkill /f /im msedge.exe
+Remove-Item  -Recurse "C:\Program Files (x86)\Microsoft\edge*"
+Write-Output "Microsoft Edge has been uninstalled until the next update. Enjoy!"
